7d811f2bfe6afd77feb9e8f7f576eeb4cb85674d
1renji <naoufalhary@gmail.com>
2024-04-15 14:08:21 +0100

    FavoriteDb.java update

--- app/src/main/java/esisa/ac/ma/projet_natif/dal/FavoriteDb.java
+++ app/src/main/java/esisa/ac/ma/projet_natif/dal/FavoriteDb.java
@@ -1,12 +1,30 @@
 package esisa.ac.ma.projet_natif.dal;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import esisa.ac.ma.projet_natif.entities.Favorite;
 
 @Database(entities = {Favorite.class}, version = 1)
 public abstract class FavoriteDb extends RoomDatabase {
-public abstract FavoriteDao favoriteDao();
+    private static volatile FavoriteDb INSTANCE;
+
+    public abstract FavoriteDao favoriteDao();
+
+    public static FavoriteDb getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (FavoriteDb.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    FavoriteDb.class, "favorite_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
 
