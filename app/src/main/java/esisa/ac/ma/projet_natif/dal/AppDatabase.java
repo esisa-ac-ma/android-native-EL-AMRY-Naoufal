5f73251c88b1bd6743ab12c3e8b8ec1fab015539
1renji <naoufalhary@gmail.com>
2024-04-15 14:06:38 +0100

    AppDatabase.java new class

--- app/src/main/java/esisa/ac/ma/projet_natif/dal/AppDatabase.java
+++ app/src/main/java/esisa/ac/ma/projet_natif/dal/AppDatabase.java
@@ -1,2 +1,27 @@
-package esisa.ac.ma.projet_natif.dal;public class AppDatabase {
+package esisa.ac.ma.projet_natif.dal;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import esisa.ac.ma.projet_natif.dal.FavoriteDao;
+import esisa.ac.ma.projet_natif.entities.Favorite;
+
+@Database(entities = {Favorite.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static volatile AppDatabase instance;
+
+    public abstract FavoriteDao favoriteDao();
+
+    public static synchronized AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "app_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
 }
