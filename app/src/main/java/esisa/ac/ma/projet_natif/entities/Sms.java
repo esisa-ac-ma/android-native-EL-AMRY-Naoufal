b45acf3e08b21878e934e7437304173cc4796e0f
1renji <naoufalhary@gmail.com>
2024-04-16 23:48:58 +0100

    Sms.java update

--- app/src/main/java/esisa/ac/ma/projet_natif/entities/Sms.java
+++ app/src/main/java/esisa/ac/ma/projet_natif/entities/Sms.java
@@ -1,4 +1,40 @@
 package esisa.ac.ma.projet_natif.entities;
 
 public class Sms {
+    private String sender;
+    private String message;
+    private long timestamp;
+
+    public Sms() {
+    }
+
+    public Sms(String sender, String message, long timestamp) {
+        this.sender = sender;
+        this.timestamp = timestamp;
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
